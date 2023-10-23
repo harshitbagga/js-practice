f4477fc915852f42c5953c6c86aed43553c97730
Harshit Bagga <50080343+harshitbagga@users.noreply.github.com>
2023-10-23 07:09:12 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,3 +4,19 @@
 
 console.log(3+3) //code readibility is really important 
 console.log("Hello")
+
+
+let name ="harshit";
+let age = 24;
+let isloggedin = false;
+let state;
+
+//int has a range of 2 to the power 53
+//string needs ""
+//bool is to validate using true or false
+// null for an empty value not undefined
+//no value will lead to undefined OR UNASSIGNED VALUE
+//symbol used for uniqueness
+//object 
+
+console.log(typeof undefined);
