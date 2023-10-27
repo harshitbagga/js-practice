1188e79b84cc64f5dc139aa805a14aac7496c8c6
Harshit Bagga <50080343+harshitbagga@users.noreply.github.com>
2023-10-27 09:36:52 +0000

    Day 3

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,91 @@
+let age  = "harshit";
+
+
+// console.log(typeof age);
+// console.log(typeof(age));
+
+let valinnumber  = Number(age) //this converts the vairable age to number make sure write datatype with capital first letter
+// console.log(typeof valinnumber);
+// console.log(valinnumber);
+
+/*Node.js v20.8.0
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+undefined
+undefined
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+number
+number
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+\string
+string
+number
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+string
+string
+number
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+string
+string
+number
+24
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+object
+object
+number
+0
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+undefined
+undefined
+number
+NaN
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+boolean
+boolean
+number
+1
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+boolean
+boolean
+number
+1
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ cd 01_basicsnoiscis
+bash: cd: 01_basicsnoiscis: No such file or directory
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+boolean
+boolean
+number
+0
+@harshitbagga ➜ /workspaces/js-practice/01_basics (main) $ node 03_ConversionOperations.js
+string
+string
+number
+NaN*/
+
+/*********************************operationns******************************** */
+
+ let str1  = "hello"
+ let str2 = "harshit"
+
+let str3  = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4)*5%3);
+
+
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"")
+
+let n1,n2,n3
+
+n1=n2=n3 = 2 + 2
+
+// console.log(n1);
---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,10 @@
+
+// console.log("2"<1);
+// console.log("02">1);
+// console.log(null<0);
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(undefined  == 0);
+
+console.log("2" == 2); //this simply compares as the "2" automatically converts to string and then it checks 
+console.log("2" === 2);// but in this case this is strict checking it not only checks its values it also checks its datatype
