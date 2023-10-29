7fac8d0b45f955c0df8d81a547bf3acfa33e925b
Harshit Bagga <50080343+harshitbagga@users.noreply.github.com>
2023-10-29 09:37:18 +0000

    math

---
+++ 01_basics/06_math.js
@@ -0,0 +1,24 @@
+const score = 400
+const balance  = new Number(100)
+// console.log(balance)
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 26.8669
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds  = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/*Random function */
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 20
+const max = 40
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
