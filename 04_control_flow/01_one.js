49e9db64923372650db1e49abe363fd4a297e640
Harshit Bagga <50080343+harshitbagga@users.noreply.github.com>
2023-12-10 15:41:32 +0000

    updated

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,36 @@
+// if statement
+// const isUserloggedIn = true
+// //conditions <,>,<=,>=,==,!=,===,!==
+// if(2!=="2"){
+// console.log("executed");
+
+// }
+// else{
+//     console.log("not executed");
+// }
+
+
+//  const balance = 1000
+// // single line if statement
+// // if (balance > 500) console.log("test"),console.log("test2");
+
+
+// if (balance<500) {
+//     console.log("less than 500");
+
+// }
+// else if (balance<750) {
+//     console.log("less than 750");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+
+// const userloggedIn = true
+// const debitCard = true
+
+// if (userloggedIn && debitCard && true) {
+// console.log("acces to card granted");
+// }
+
---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,24 @@
+const month = 3
+
+switch (month) {
+    case 1: console.log("Jan");
+        
+        break;
+        
+    case 2: console.log("Feb");
+        
+        break;
+        
+    case 3: console.log("mar");
+        
+        break;
+        
+    case 4: console.log("apr");
+        
+        break;
+        
+
+    default:
+        console.log("end of life");
+        break;
+}
