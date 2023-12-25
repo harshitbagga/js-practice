c8f52bb29a122b712b7eca0dc0442fb1fb9edaae
Harshit Bagga <50080343+harshitbagga@users.noreply.github.com>
2023-12-25 10:17:57 +0000

    while & do while

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,17 @@
+let index  = 0
+while(index <= 10){
+console.log(`the value of index is ${index}`)
+index = index +1
+}
+let myarr = ["flash", "batman", "superman", "wonder women"]
+
+let arr = 0
+while(arr<myarr.length){
+console.log(`value is ${myarr[arr]}`)
+arr = arr+1
+}
+let score  = 0
+do{
+console.log(`score is ${score}`);
+score++
+} while(score <=10);
