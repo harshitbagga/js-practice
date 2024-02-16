9c7778b50ccc1d0b0a4370f72205ef754a2bafe2
Harshit Bagga <50080343+harshitbagga@users.noreply.github.com>
2024-02-16 08:20:56 +0000

    objects constructor done

--- 10_classes_and _oop/oops.js
+++ 10_classes_and _oop/oops.js
@@ -1,16 +1,41 @@
+//OBJECT LITERAL
+
 const user = {
     username: "hitesh",
-    loginCount : 8,
-    signedIn: true,
+    logincount : 8,
+    isloggedin: true,
 
-    getUserdetails: function() {
-       // console.log("User details received");
-      console.log(`username:${this.username}`);
-    }
+    // getUserdetails: function() {
+    //    // console.log("User details received");
+    //   console.log(`username:${this.username}`);
+    // }
 }
 
-console.log(user.username);
-console.log(user.getUserdetails());
+// console.log(user.username);
+// //console.log(user.getUserdetails());
+// console.log(this);
 
 
+//CONSTRUCTION FUNCTION
 
+// const promiseone = new Promise()
+// const date   = new Date()
+
+function User(username, logincount, isloggedin) {
+  this.username = username 
+  this.logincount = logincount
+  this.isloggedin = isloggedin
+  return this
+}
+
+this.greeting - function () {
+  console.log(`Welcome ${this.username}`);
+}
+
+const userone = new User("harshit", 24, true) 
+//new keyword is very important otherwise it will show old value even if you create new const 
+//new keyword creates new object and calls the function like here its calling User function
+
+const usertwo = new User("chai", 23, false)
+console.log(userone.constructor);
+console.log(usertwo);
